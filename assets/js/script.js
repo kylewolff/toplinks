ecf758d2a9dd4fb3e6f48436503a321ae8e7cfa0
kylewolff <kyle.wolff7@gmail.com>
2014-11-23 16:09:56 -0600

    Add about page AJAX

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-$(document).ready(function{});
+$(function aboutLoad(){
+    // don't cache ajax or content won't be fresh
+    $.ajaxSetup ({
+        cache: false
+    });
+      var ajax_load = "<img src='http://automobiles.honda.com/images/current-offers/small-loading.gif' alt='loading...' />";
+    // load() functions
+    var loadUrl = "http://fiddle.jshell.net/kylewolff7/hor54tj8/show/";
+    $("#about").click(function(){
+        $("#mainContent").html(ajax_load).load(loadUrl);
+    });
+
+// end  
+});
+
+$(function linkLoad(){
+    // don't cache ajax or content won't be fresh
+    $.ajaxSetup ({
+        cache: false
+    });
+      var ajax_load1 = "<img src='http://automobiles.honda.com/images/current-offers/small-loading.gif' alt='loading...' />";
+    // load() functions
+    var loadUrl1 = "http://fiddle.jshell.net/kylewolff7/ovger45n/show/";
+    $("#logo").click(function(){
+        $("#mainContent").html(ajax_load1).load(loadUrl1);
+    });
+
+// end  
+});
